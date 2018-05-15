b345fd0e941847897569bb57b07f63de8f240eec
Daftus <lemouzy.c@hotmail.com>
2018-05-15 23:28:51 +0200

    Add files via upload

--- src/Landing.js
+++ src/Landing.js
@@ -2,36 +2,34 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+
 const Landing = ({ data: { loading, allPosts = []} }) => {
- if (!loading) {
+ if (!loading) {console.log(allPosts);
   return (
+
    <div className="wrapper">
-    {allPosts.map(post => ( 
+    {allPosts.map(post => (
      <article className="content" key={post.id}>
-      <h2>{post.title}</h2>
-      <p dangerouslySetInnerHTML={{ __html: post.description }} />
-      <Link to={`/${post.id}`}>
-       <button className="btn">Read More</button>
-      </Link>
+       <Link to={`/${post.id}`}>
+        <h2>{post.title}</h2>
+        <p>ID : {post.id}</p>
+        <img src="" alt="Avatar" /><br/><br/>
+       <button className="btn">Détails</button>
+      </Link><br/><br/>
      </article>
     ))}
-
    </div>
-  );
+  ); 
  }
  return <h2>Loading Posts...</h2>
 };
-const allPosts = gql`
- query allPosts {
-  allPosts(orderBy: date_ASC) {
-    id
-    title
-    date
-    location
-    image {
-      url
-    }
-  }
+
+const allPosts = gql` query allPosts {
+  allPosts (orderBy: createdAt_ASC) {
+   id
+   title
+   content
+  } 
 }
 `;
 export default graphql(allPosts)(Landing);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, StaticRouter, Switch } from 'react-router-dom'
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo'
-import Header from './Header';
-import Landing from './Landing';
 import registerServiceWorker from './registerServiceWorker';
-import Post from './posts/Post'
+import App from './App';
 
 const API = 'https://api.graphcms.com/simple/v1/cjh6g6zae17ym0110gflz68af';
 
@@ -19,15 +16,7 @@ const client = new ApolloClient({
 
 ReactDOM.render(
  <ApolloProvider client={client}>
-    <BrowserRouter>
-      <main>
-	  <Header />
-	  <Switch>
-	   <Route exact path="/" component={Landing} />
-	   <Route path="/:id" component={Post} />
-	  </Switch>
-	 </main>
-    </BrowserRouter>
+    <App />
  </ApolloProvider>,
 document.getElementById('root'));
 registerServiceWorker(); 
